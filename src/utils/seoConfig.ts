3aa2fcbc3ec18eb99961c1b31e176d2504fee37e
DVelandia <dfvf03@gmail.com>
2024-07-18 18:07:27 -0500

    add: OpenGraph image

--- src/utils/seoConfig.ts
+++ src/utils/seoConfig.ts
@@ -7,10 +7,10 @@ export const seoConfig = {
 		"Portfolio de Daniel Velandia, desarrollador web full stack especializado en Drupal y WordPress. Estudiante de Ingeniería de Sistemas y apasionado por la innovación tecnológica.",
 	type: "website",
 	image: {
-		url: "https://portfolio-psi-fawn-19.vercel.app/og.jpg",
+		url: "https://portfolio-psi-fawn-19.vercel.app/og.png",
 		alt: "Daniel Velandia - Desarrollador Web",
-		width: 495,
-		height: 339,
+		width: 1200,
+		height: 630,
 	},
 	siteName: "Portfolio de Daniel Velandia",
 	twitter: {
